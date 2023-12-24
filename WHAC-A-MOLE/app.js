b278a77c1af64273e67f8408f75051bcc5f313ff
Sujeet Kumar <88089946+sujeet-kr1415@users.noreply.github.com>
2023-12-24 13:15:43 +0000

    bit change

--- WHAC-A-MOLE/app.js
+++ WHAC-A-MOLE/app.js
@@ -1,4 +1,4 @@
-const squares = document.querySelector('.square')
+const squares = document.querySelectorAll('.square')
 const mole = document.querySelector('.mole')
 const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
